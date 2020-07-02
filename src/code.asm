97beca3212e0305b41a43fa351759bf06a772a7e
Billy Stevens <contact@wasv.me>
2020-07-02 18:46:26 -0400

    Adds include example.

--- src/code.asm
+++ src/code.asm
@@ -1,13 +1,16 @@
-    RESB 0x100
     cpu 8086
 
+%include "inc/dos.inc"
+
+    RESB 0x100
 section code
 
 extern msg
+
 global ..start
 ..start:
     MOV dx,msg
-    MOV ah,9
+    MOV ah,FC_PUTS
     INT 21h
     MOV ah,4Ch
     INT 21h
